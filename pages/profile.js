62de3506e9abb3b607317ef000d93172470854cd
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-06-17 18:36:52 +0900

    🟢 팔로잉 팔로워 목록 보여주기 grid 사용

--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { AppLayout } from '../components/AppLayout'
+import Head from 'next/head'
+import NicknameEditForm from '../components/NickNameEditForm'
+import FollowList from '../components/FollowList'
 
 export default function profile() {
+  const followingList = [{nickname:'베드로'},{nickname:'요한'},{nickname:'야고보'}];
+  const followerList = [{nickname:'베드로'},{nickname:'요한'},{nickname:'야고보'}];
+
   return (
     <>
+    <Head>
+      <title>내 프로필 | NodeBird</title>
+    </Head>
       <AppLayout>
-        My profile
+        <NicknameEditForm/>
+        <FollowList header="팔로잉 목록" data={followingList}/>
+        <FollowList header="팔로워 목록" data={followerList}/>
       </AppLayout>
     </>
   )
