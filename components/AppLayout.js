2e1e6a3fc3b0c157d40cb692c2ae21b85eb2a534
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-06-17 13:50:15 +0900

    next/link로 페이지 이동

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Link from 'next/link'
 export const AppLayout = ({children}) => {
   return (
     <div>
-        <div>공통메뉴</div>
+        <Link href="/">홈으로</Link>
+        <Link href="/profile">내 정보</Link>
+        <Link href="/signup">회원가입</Link>
         {children}
     </div>
   )
