d71a515e00570624995a71e25937941f0a84c69b
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-06-17 14:56:07 +0900

    antd 사용하여 메뉴 만들기 문제 : css 적용이 안된 상태

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Link from 'next/link'
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+
+const items=[
+  {
+    label: 'Navigation One',
+    key: 'mail',
+    icon: <MailOutlined />,
+  },
+]
+
 export const AppLayout = ({children}) => {
+  const [current, setCurrent] = useState('mail');
+
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
   return (
     <div>
+        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
         <Link href="/">홈으로</Link>
         <Link href="/profile">내 정보</Link>
         <Link href="/signup">회원가입</Link>
